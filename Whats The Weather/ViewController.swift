cd2f90adf37102787d1fc2c1912931f4300421ad
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-07-26 13:46:23 +0200

    Connections between layout and code

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityFextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
